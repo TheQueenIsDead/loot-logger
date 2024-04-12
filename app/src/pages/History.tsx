77543e2ae402e07451182d02f6ff9b7a74106423
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-04-12 20:22:18 +1200

    feat: rename Tab[1:3] to Log, History, and Settings

--- app/src/pages/History.tsx
+++ app/src/pages/History.tsx
@@ -1,25 +1,25 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import './History.css';
 
-const Tab2: React.FC = () => {
+const History: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>History</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">History</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <ExploreContainer name="History" />
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default History;
