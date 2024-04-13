7a38d3cad75fc421b055ab8cd862c3b02a0acbc0
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-04-14 01:49:23 +1200

    feat: add a data store singleton service in order to save and persist settings config page

--- app/src/components/SettingsContainer.tsx
+++ app/src/components/SettingsContainer.tsx
@@ -1,48 +1,56 @@
 import './SettingsContainer.css';
 
-import {Preferences} from '@capacitor/preferences'
-import {IonAlert, IonButton, IonInput, IonItem, IonLabel, IonList} from "@ionic/react";
-import React, {useEffect, useState} from "react";
-import {Storage} from "@ionic/storage";
-import GetStore from "../lib/store";
+import {IonAlert, IonInput, IonItem, IonLabel, IonList} from "@ionic/react";
+import {useEffect, useState} from "react";
+import {StorageService} from '../services/Storage';
 
-const setSalary = async () => {
-    await Preferences.set({
-        key: 'salary',
-        value: '50000',
-    });
-};
+type Settings = {
+    salary: number,
+    hours: number,
+    wage: number | null,
+}
 
 
 const SettingsContainer: React.FC = () => {
 
     const [yearlySalary, setYearlySalary] = useState<number>(0);
     const [weeklyHours, setWeeklyHours] = useState<number>(0);
-    const [hourlyWage, setHourlyWage] = useState<number | null>(null);
+    const [hourlyWage, setHourlyWage] = useState<number | null>(0);
     const [showAlert, setShowAlert] = useState<boolean>(false);
+
+
+    StorageService.getInstance().then(store => {
+        console.log("Managed to get db:" + JSON.stringify(store));
+        store.getConfig().then(res => {
+            console.log(JSON.stringify(res))
+            setYearlySalary(res.salary)
+            setWeeklyHours(res.hours)
+            setHourlyWage(res.wage)
+        })
+    }).catch(err => {
+        console.log("Failed to get db: " + err.toString())
+    })
+
 
     // Calculate the hourly wage when yearly salary or weekly hours is updated
     useEffect(() => {
         calculateHourlyWage();
     }, [yearlySalary, weeklyHours]);
 
-    // Save settings to the ionic store when an hourly wage is successfully calculated
-    useEffect(() => {
-        persistSettings();
-    }, [hourlyWage]);
+    // // Save settings to the ionic store when an hourly wage is successfully calculated
+    // useEffect(() => {
+    //     StorageService.getInstance().then(store => {
+    //         store.setConfig(yearlySalary, weeklyHours, hourlyWage)
+    //     })
+    // }, [hourlyWage]);
 
-    const persistSettings: any = async() => {
-        const store = await GetStore();
-        await Promise.all([
-            store.set('salary', yearlySalary),
-            store.set('hours', weeklyHours),
-            store.set('wage', hourlyWage),
-        ])
-    }
+
     const calculateHourlyWage = () => {
         if (yearlySalary > 0 && weeklyHours > 0) {
             const hourlyWageResult = yearlySalary / (52 * weeklyHours);
-            setHourlyWage(hourlyWageResult);
+            if (hourlyWageResult !== hourlyWage) {
+                setHourlyWage(hourlyWageResult);
+            }
         } else {
             setShowAlert(true);
         }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import GetStore from './lib/store'
-
-// Call GetStore on app startup in order to initialise the singleton
-await GetStore()
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
