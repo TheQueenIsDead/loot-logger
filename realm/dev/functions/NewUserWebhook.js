85d88f6629b94afa7494544fd87f396fc7c46f05
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-04-25 14:11:50 +1200

    deploy: add new user webhook to post to discord on user signup

---
+++ realm/dev/functions/NewUserWebhook.js
@@ -0,0 +1,20 @@
+
+
+exports = async function() {
+  
+  const user = context.user
+
+  const webhook = context.values.get("webhook-dev");
+  
+  const response = await context.http.post({
+    url: webhook,
+    body: { 
+      content: JSON.stringify(user),
+      username: "Mongo",
+      avatar_url: "https://1000logos.net/wp-content/uploads/2020/08/MongoDB-Emblem-500x313.jpg"
+    },
+    encodeBodyAsJSON: true
+  })
+  // The response body is a BSON.Binary object. Parse it and return.
+  return EJSON.parse(response.body.text());
+};
