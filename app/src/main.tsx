94c51dad72998aed48b899a970a35fc13dae4f3d
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-04-26 12:02:30 +1200

    feat: add sentry monitoring for capacitor and react

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,22 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+import * as Sentry from "@sentry/capacitor";
+import * as SentryReact from "@sentry/react";
+import {BrowserTracing} from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://8fe32e4624f58f6e753fe034b5022d81@o4507146805772288.ingest.de.sentry.io/4507146808655952",
+    // release: "",
+    // dist: "",
+    tracesSampleRate: 1,
+    integrations: [ new BrowserTracing() ]
+}, SentryReact.init);
+
+
+
+// Sentry.captureException("Test Captured Exception");
+
 const container = document.getElementById('root');
 const root = createRoot(container!);
 root.render(
