a08f750109b88f16db17a3ee711c9dcfb839ea51
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-04-26 14:58:10 +1200

    wip: attempt to initialise different sentry modules based on capacitor platform

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -39,18 +39,6 @@ import UserManagement from './UserManagement';
 
 setupIonicReact();
 
-import * as Sentry from "@sentry/capacitor";
-import * as SentryReact from "@sentry/react";
-
-Sentry.init({
-  dsn: "https://8fe32e4624f58f6e753fe034b5022d81@o4507146805772288.ingest.de.sentry.io/4507146808655952",
-  // release: "",
-  // dist: "",
-  tracesSampleRate: 1,
-  integrations: [],
-  debug: true
-}, SentryReact.init);
-
 const App: React.FC = () => {
   return (
   <IonApp>
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -2,6 +2,42 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+import * as SentryCapacitor from "@sentry/capacitor";
+import * as SentryReact from "@sentry/react";
+import {BrowserTracing} from "@sentry/react";
+
+import {Capacitor} from "@capacitor/core";
+
+
+
+switch (Capacitor.getPlatform()) {
+    case "web":
+        console.log("initialising sentry react")
+        // @ts-ignore
+        SentryReact.init({
+            dsn: "https://8fe32e4624f58f6e753fe034b5022d81@o4507146805772288.ingest.de.sentry.io/4507146808655952",
+            // release: "",
+            // dist: "",
+            tracesSampleRate: 1,
+            integrations: [new BrowserTracing()],
+            // debug: true
+        })
+        break
+    case "android":
+        console.log("initialising sentry capacitor")
+        SentryCapacitor.init({
+            dsn: "https://8fe32e4624f58f6e753fe034b5022d81@o4507146805772288.ingest.de.sentry.io/4507146808655952",
+            // release: "",
+            // dist: "",
+            tracesSampleRate: 1,
+            integrations: [],
+            debug: true
+        })
+        break
+    default: // default and ios
+        break
+}
+
 
 
 const container = document.getElementById('root');
