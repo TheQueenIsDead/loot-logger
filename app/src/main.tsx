684634dddc37442c15b040ec9e7e201ae1e092a0
TheQueenIsDead <30706552+TheQueenIsDead@users.noreply.github.com>
2024-04-26 13:37:59 +1200

    deps: downgrade @sentry/capacitor to 0.15.0 and @sentry/react to 7.81.1 to mitigate bug
     - ref: https://github.com/getsentry/sentry-capacitor/issues/634

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -4,19 +4,15 @@ import App from './App';
 
 import * as Sentry from "@sentry/capacitor";
 import * as SentryReact from "@sentry/react";
-import {BrowserTracing} from "@sentry/react";
 
 Sentry.init({
     dsn: "https://8fe32e4624f58f6e753fe034b5022d81@o4507146805772288.ingest.de.sentry.io/4507146808655952",
     // release: "",
     // dist: "",
     tracesSampleRate: 1,
-    integrations: [ new BrowserTracing() ]
+    integrations: [],
+    debug: true
 }, SentryReact.init);
-
-
-
-// Sentry.captureException("Test Captured Exception");
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
